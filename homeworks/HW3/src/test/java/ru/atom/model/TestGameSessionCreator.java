603ac5860af7de9256f5c2ac7f8177f915c5904a
poplavk <l.poplavkova@gmail.com>
2017-04-30 17:01:18 +0300

    add classes from hw2

---
+++ homeworks/HW3/src/test/java/ru/atom/model/TestGameSessionCreator.java
@@ -0,0 +1,26 @@
+package ru.atom.model;
+
+/**
+ * Create sample game session with all kinds of objects that will present in bomber-man game
+ */
+public final class TestGameSessionCreator {
+    private TestGameSessionCreator() {
+    }
+
+    static GameSession createGameSession() {
+        GameSession gameSession = new GameSession();
+        gameSession.addGameObject(new Brick(0, 0, Brick.BrickType.UNBREACABLE));
+        gameSession.addGameObject(new Brick(0, 4, Brick.BrickType.UNBREACABLE));
+
+        gameSession.addGameObject(new Girl(0, 2));
+        gameSession.addGameObject(new Girl(1, 2));
+
+        gameSession.addGameObject(new Bomb(2, 2, 15));
+        gameSession.addGameObject(new Bomb(3, 3, 15));
+
+        gameSession.addGameObject(new Bonus(4, 4, Bonus.BonusType.FIRE));
+        gameSession.addGameObject(new Bonus(5, 5, Bonus.BonusType.SPEED));
+
+        return gameSession;
+    }
+}
