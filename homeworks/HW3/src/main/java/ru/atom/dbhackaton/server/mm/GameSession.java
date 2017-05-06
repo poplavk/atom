f44d831cff18ceac2a9157f565e3e11c72ce5141
poplavk <l.poplavkova@gmail.com>
2017-05-06 21:30:22 +0300

    add classes from dbhackaton

---
+++ homeworks/HW3/src/main/java/ru/atom/dbhackaton/server/mm/GameSession.java
@@ -0,0 +1,47 @@
+package ru.atom.dbhackaton.server.mm;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.atom.dbhackaton.server.base.Match;
+import ru.atom.dbhackaton.server.service.MatchMakerService;
+
+import java.util.Arrays;
+
+public class GameSession {
+    private static final Logger logger = LogManager.getLogger(GameSession.class);
+
+//    private static AtomicLong idGenerator = new AtomicLong();
+
+    public static final int PLAYERS_IN_GAME = 4;
+
+    private final Connection[] connections;
+
+    private final Integer id;
+
+
+
+    public GameSession(Connection[] connections) {
+        this.id = MatchMakerService.saveMatch(new Match());
+        this.connections = connections;
+        sendIdToConnections();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void sendIdToConnections(){
+        for (Connection connection: connections) {
+            connection.setSessionId(id);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "GameSession{" +
+                "connections=" + Arrays.toString(connections) +
+                ", id=" + id +
+                '}';
+    }
+}
