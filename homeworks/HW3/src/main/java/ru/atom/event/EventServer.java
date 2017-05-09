0efa1f0f80bceb23e5d00467c5c2a1f1b67bece1
poplavk <l.poplavkova@gmail.com>
2017-05-10 00:43:32 +0300

    10.05

--- homeworks/HW3/src/main/java/ru/atom/event/EventServer.java
+++ homeworks/HW3/src/main/java/ru/atom/event/EventServer.java
@@ -2,8 +2,11 @@ package ru.atom.event;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import ru.atom.dbhackaton.server.CrossBrowserFilter;
 
 public class EventServer {
     public static Server server;
@@ -11,7 +14,7 @@ public class EventServer {
     public static void start() {
         server = new Server();
         ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8090);
+        connector.setPort(8089);
         server.addConnector(connector);
 
         // Setup the basic application "context" for this application at "/"
@@ -32,6 +35,39 @@ public class EventServer {
             t.printStackTrace(System.err);
         }
     }
+
+
+//    private static ServletContextHandler createGsContext() {
+//        ServletContextHandler context = new ServletContextHandler();
+//        context.setContextPath("/");
+//        ServletHolder jerseyServlet = context.addServlet(
+//                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+//        jerseyServlet.setInitOrder(0);
+//
+//        jerseyServlet.setInitParameter(
+//                "jersey.config.server.provider.packages",
+//                "ru.atom.bombergirl.gameserver"
+//        );
+//
+//        jerseyServlet.setInitParameter(
+//                "com.sun.jersey.spi.container.ContainerResponseFilters",
+//                CrossBrowserFilter.class.getCanonicalName()
+//        );
+//
+//        return context;
+//    }
+//
+//    private static ContextHandler createResourceContext() {
+//        ContextHandler context = new ContextHandler();
+//        context.setContextPath("/gs/0/*");
+//        ResourceHandler handler = new ResourceHandler();
+//        String eventRoot = EventServer.class.getResource("/static").toString();
+//        String serverRoot = eventRoot.substring(0, eventRoot.length() - 35) + "frontend/src/main/webapp";
+//        handler.setWelcomeFiles(new String[]{serverRoot + "index.html"});
+//        handler.setResourceBase(serverRoot);
+//        context.setHandler(handler);
+//        return context;
+//    }
 
     public static void main(String[] args) {
         start();
