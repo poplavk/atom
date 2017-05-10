f532c590de3feb82d3d67d096795911ff7cd450a
Dmitry Bragin <dmbragin@esc-center.ru>
2017-05-10 16:18:59 +0300

    Move some logic from GameSession to Ticker; Ticker now  is runnable

--- homeworks/HW3/src/main/java/ru/atom/model/GameSession.java
+++ homeworks/HW3/src/main/java/ru/atom/model/GameSession.java
@@ -5,22 +5,21 @@ import org.apache.logging.log4j.Logger;
 import ru.atom.dbhackaton.server.base.Match;
 import ru.atom.dbhackaton.server.mm.Connection;
 import ru.atom.dbhackaton.server.service.MatchMakerService;
+import ru.atom.geometry.Point;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class GameSession implements Tickable, Runnable {
+public class GameSession implements Tickable {
     private static final Logger logger = LogManager.getLogger(ru.atom.dbhackaton.server.mm.GameSession.class);
 //    private static AtomicLong idGenerator = new AtomicLong();
-    public static final int PLAYERS_IN_GAME = 4;
-    private final Connection[] connections;
-    private final Integer id;
+
     private static final Logger log = LogManager.getLogger(GameSession.class);
     private List<GameObject> gameObjects = new ArrayList<>();
     private static int lastId = 0;
 
-
+    //TODO мне ненравится
     public static int getNextId() {
         return lastId++;
     }
@@ -34,28 +33,16 @@ public class GameSession implements Tickable, Runnable {
         log.info("add new game object {}", gameObject.getClass().toString());
     }
 
+
+    private void generateMap() {
+        // TODO add map generator
+        addGameObject(new Bonus(new Point(4, 4), Bonus.BonusType.FIRE));
+        addGameObject(new Bonus(new Point(5, 5), Bonus.BonusType.SPEED));
+    }
+
+
     public GameSession() {
-        this.id = MatchMakerService.saveMatch(new Match());
-        this.connections = new Connection[PLAYERS_IN_GAME];
-        sendIdToConnections();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void sendIdToConnections(){
-        for (Connection connection: connections) {
-//            connection.setSessionId(id);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "GameSession{" +
-                "connections=" + Arrays.toString(connections) +
-                ", id=" + id +
-                '}';
+        generateMap();
     }
 
     @Override
@@ -70,16 +57,11 @@ public class GameSession implements Tickable, Runnable {
             if (gameObject instanceof Temporary && ((Temporary) gameObject).isDead()) {
                 dead.add((Temporary) gameObject);
                 if (gameObject instanceof Bomb) {
-                    newObjects.addAll( ((Bomb) gameObject).getBlast());
+                    newObjects.addAll(((Bomb) gameObject).getBlast());
                 }
             }
         }
         gameObjects.removeAll(dead);
         gameObjects.addAll(newObjects);
-    }
-
-    @Override
-    public void run() {
-
     }
 }
